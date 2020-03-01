5f1280d812898a4c442ee7513cc7f22cd86d072f
Samruddhi Jadhav <samruddhi.jadhav@paytmmoney.com>
2020-03-01 10:48:20 +0530

    1. Tests WIP, changed bundle id

--- SignerUITests/SignerUITests.swift
+++ SignerUITests/SignerUITests.swift
@@ -21,6 +21,12 @@ class SignerUITests: XCTestCase {
     }
 
     func testSetupAndAccount() {
-        let firstName = app.textFields.element(matching: .textField, identifier: "firstName")
+        let setupButton = app.buttons.element(matching: .button, identifier: "SetupButton")
+        if setupButton.waitForExistence(timeout: 5.0) {
+            let privateKey = app.textFields.element(matching: .textField, identifier: "privateKeyLabel")
+            privateKey.tap()
+            privateKey.typeText("0xa2ae25c5b887bb427e6adc7cccbbd10329c80ae8cd81c25675b26a06aa669ba1")
+            setupButton.tap()
+        }
     }
 }
