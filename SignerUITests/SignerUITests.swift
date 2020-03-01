31c42adf42322b81bcaceedb20d0c0479eb499da
Samruddhi Jadhav <samruddhi.jadhav@paytmmoney.com>
2020-03-01 11:15:05 +0530

    1. UITests finished

--- SignerUITests/SignerUITests.swift
+++ SignerUITests/SignerUITests.swift
@@ -20,13 +20,55 @@ class SignerUITests: XCTestCase {
         app.launch()
     }
 
-    func testSetupAndAccount() {
+    func testSetup() {
         let setupButton = app.buttons.element(matching: .button, identifier: "SetupButton")
         if setupButton.waitForExistence(timeout: 5.0) {
             let privateKey = app.textFields.element(matching: .textField, identifier: "privateKeyLabel")
             privateKey.tap()
             privateKey.typeText("0xa2ae25c5b887bb427e6adc7cccbbd10329c80ae8cd81c25675b26a06aa669ba1")
             setupButton.tap()
+        }
+    }
+
+    func testAccount() {
+        testSetup()
+
+        let signButton = app.buttons.element(matching: .button, identifier: "SignButton")
+        let verifyButton = app.buttons.element(matching: .button, identifier: "VerifyButton")
+
+        if signButton.waitForExistence(timeout: 5.0) && verifyButton.waitForExistence(timeout: 5.0) {
+            XCTAssert(app.staticTexts["Address"].exists)
+            XCTAssert(app.staticTexts["Balance"].exists)
+        }
+    }
+
+    func testSign() {
+        testAccount()
+
+        let signButton = app.buttons.element(matching: .button, identifier: "SignButton")
+        if signButton.waitForExistence(timeout: 5.0) {
+            signButton.tap()
+            let message = app.textFields.element(matching: .textField, identifier: "messageTextFiled")
+            message.tap()
+            message.typeText("message")
+
+            let signMessageButton = app.buttons.element(matching: .button, identifier: "SignMessageButton")
+            signMessageButton.tap()
+        }
+    }
+
+    func testVerify() {
+        testAccount()
+
+        let verifyButton = app.buttons.element(matching: .button, identifier: "VerifyButton")
+        if verifyButton.waitForExistence(timeout: 5.0) {
+            verifyButton.tap()
+            let message = app.textFields.element(matching: .textField, identifier: "MessageTextField")
+            message.tap()
+            message.typeText("message")
+
+            let signMessageButton = app.buttons.element(matching: .button, identifier: "VerifyMessageButton")
+            signMessageButton.tap()
         }
     }
 }
